c152a8146fd9fdce28036b07270b61d3cd68514f
heypurnomo <heypurnomo@gmail.com>
2022-04-10 21:47:04 +0700

    feat: session login

--- server/routers/v1/index.js
+++ server/routers/v1/index.js
@@ -1,6 +1,8 @@
 const v1 = require('express').Router();
 const product = require('./productRouter')
+const auth = require('./authRouter')
 
+v1.use('/', auth)
 v1.use('/product', product)
 
 module.exports = v1
