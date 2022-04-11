361da8544c991767f92f8b05ecfaddff82454f8c
heypurnomo <heypurnomo@gmail.com>
2022-04-11 15:00:55 +0700

    fix: authentication

--- server/middleware/isAuthenticated.js
+++ server/middleware/isAuthenticated.js
@@ -3,9 +3,9 @@ const { verifyToken } = require("../utils/tokenHandler");
 module.exports = (req, res, next) => {
   try {
     const { token } = req.cookies;
-    if (!token) res.status(401).json({message: 'Unauthorized'});
+    if (!token) return res.status(401).json({message: 'Unauthorized'});
     const data = verifyToken(token);
-    if (!data) res.status(401).json({message: 'Unauthorized'});
+    if (!data) return res.status(401).json({message: 'Unauthorized'});
     req.user = data;
     next();
   } catch (error) {
