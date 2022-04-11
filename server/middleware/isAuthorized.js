285a31216a493f55dc2b527eb69ee3020e598b6d
heypurnomo <heypurnomo@gmail.com>
2022-04-12 06:42:54 +0700

    fix: ngesimpelin argumen

--- server/middleware/isAuthorized.js
+++ server/middleware/isAuthorized.js
@@ -1,15 +1,22 @@
 /**
  * 
- * @options [{role: rolenya apa, sameUser: boolean}]
+ * @options 
+ * [{role: 'admin'}, {eole:'user', sameUser: true}]
+ * admin biasa akses punya sipa saja, user hanya bisa akses punya sendiri
+ * [{sameUser: true}]
+ * hanya bisa akses punya sendiri siapapun itu
  */
 
 module.exports = (options) => {
   return (req, res, next) => {
     const {role, id} = req.user;
     const uid = req.query.userId || req.params.id;
-    console.log(uid, id)
     let isAuthorized = false;
     for (const opt of options) {
+      if (!opt.role && opt.sameUser && (id == uid)) {
+        isAuthorized = true;
+        break;
+      }
       if (opt.role === role) isAuthorized = true;
       if (!opt.sameUser) continue;
       if (id != uid) isAuthorized = false;  
