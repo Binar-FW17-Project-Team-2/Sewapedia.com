16d727d86678b585b125e273e71b75bd285ca341
icanq <icanq@hotmail.com>
2022-04-13 21:46:50 +0700

    notes problem

--- server/controller/authentication/signUp.js
+++ server/controller/authentication/signUp.js
@@ -1,11 +1,12 @@
 const { User, Biodata } = require('../../models')
 
 class AuthController {
-    static async register (req, res) {
+    static async register(req, res) {
         try {
             const { email, password } = req.body
-            const isEmailExist = await User.findOne({ where: { email }})
-            if (isEmailExist){
+            if (!email) res.status(400).json({ message: "please input email" })
+            const isEmailExist = await User.findOne({ where: { email } })
+            if (isEmailExist) {
                 res.status(409).json({ message: 'email alrady take' })
             }
             return await User.create({
@@ -22,9 +23,11 @@ class AuthController {
                 throw new Error(error)
             })
         } catch (error) {
+            // implement errorHandler nanti refferencenya dikasih
+            // biar si servernya gk mati, jgn pake throw new Error
             throw new Error(error)
         }
     }
-}   
+}
 
-module.exports= AuthController
+module.exports = AuthController
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const routers = require('./routers')
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
