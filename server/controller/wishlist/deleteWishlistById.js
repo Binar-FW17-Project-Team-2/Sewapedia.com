1889646535f0f2754e1c7a2c6f61abb14dcc93e2
rizalherniawan <rizalherniawan@gmail.com>
2022-04-13 12:08:09 +0700

    feat: delete wishlist by id revision

--- server/controller/wishlist/deleteWishlistById.js
+++ server/controller/wishlist/deleteWishlistById.js
@@ -2,13 +2,14 @@ const { Wishlist } = require('../../models')
 
 module.exports = async(req,res) => {
     try {
-        const id = req.params.id
-        const deleteWihlist = Wishlist.destroy({
+        const userId = req.query.userId
+        const productId = req.body.productId
+        const deleteWishlist = Wishlist.destroy({
             where: {
-                id: id
+              userId: userId,
+              productId: productId
             }
         })
-        if(!deleteWihlist) return res.status(400).json([0,{message: 'this user does not have wishlist'}])
         return res.status(200).json([1, {message: 'delete success'}])
     } catch (error) {
         res.status(500).json({message: 'internal server error'})
--- server/routers/v1/wishlistRouter.js
+++ server/routers/v1/wishlistRouter.js
@@ -3,9 +3,9 @@ const { createWishlist, viewWilshlistById, deleteWishlistById } = require('../..
 const { isAuthenticated, isAuthorized } = require('../../middleware')
 
 wishlist.use(isAuthenticated)
-// wishlist.use(isAuthorized([{sameUser: true}]))
 wishlist.post('/', createWishlist)
+wishlist.use(isAuthorized([{sameUser: true}]))
 wishlist.get('/', viewWilshlistById)
-wishlist.delete('/:id', deleteWishlistById)
+wishlist.delete('/', deleteWishlistById)
 
 module.exports = wishlist
