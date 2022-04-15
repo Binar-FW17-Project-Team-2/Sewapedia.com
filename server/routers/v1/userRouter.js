cf17b702d5d580ae234e91f25103260b87a6e6d5
asyhp <aisyahap1595@gmail.com>
2022-04-15 13:26:59 +0700

    feat: edit user

---
+++ server/routers/v1/userRouter.js
@@ -0,0 +1,9 @@
+const user = require('express').Router()
+const { isAuthenticated, isAuthorized } = require('../../middleware')
+const { getUsers, getUserById, editUser } = require('../../controller/user')
+
+user.use(isAuthenticated)
+user.use(isAuthorized([{role: 'admin'}, {role: 'user'}]))
+user.post('/edit/:id', editUser)
+
+module.exports = user
