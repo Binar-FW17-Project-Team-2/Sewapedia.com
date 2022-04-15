8ed4d142c4aaa466f61c55ffb93231598464dd02
heypurnomo <heypurnomo@gmail.com>
2022-04-15 22:49:02 +0700

    feat: forgot password

--- server/routers/v1/authRouter.js
+++ server/routers/v1/authRouter.js
@@ -1,7 +1,9 @@
 const auth = require('express').Router()
-const { login, logout } = require('../../controller/authentication')
+const { login, logout, forgotPassword, resetPassword } = require('../../controller/authentication')
 
 auth.post('/login', login)
 auth.get('/logout', logout)
+auth.post('/forgotpw', forgotPassword)
+auth.post('/resetpw', resetPassword)
 
 module.exports = auth
