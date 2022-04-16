3310cd9c9b2d47248ed4a963cef9ab87ff59169e
Muhammad Rizky <mrizky98@gmail.com>
2022-04-16 21:35:20 +0700

    perbaiki conflict

--- client/sewapedia-app/src/App.js
+++ client/sewapedia-app/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import React from 'react';
+
+
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import About from './Pages/About';
 import Home from './Pages/Home';
 import FAQ from './Component/FAQ';
 import CartPage from './Pages/CartPage';
 
+
 export default function App() {
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-       
+        <Route path='/cart' element={<CartPage />} />
         <Route path='/about' element={<About />} />
         <Route path='/faq' element={<FAQ />} />
+
     </Routes>
     </BrowserRouter>
   )
