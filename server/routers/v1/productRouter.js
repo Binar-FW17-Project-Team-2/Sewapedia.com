fdfe09257ad77857f1a3dcf85ffd2b30ab5df444
rizalherniawan <rizalherniawan@gmail.com>
2022-04-17 16:13:01 +0700

    fixing routter for product

--- server/routers/v1/productRouter.js
+++ server/routers/v1/productRouter.js
@@ -10,10 +10,10 @@ const {
 const { isAuthenticated, roleAuthorization} = require('../../middleware')
 
 product.get('/', getProducts)
+product.get('/:id', getProductById)
 product.get('/category', getListCategory)
 product.use(isAuthenticated, roleAuthorization('admin'))
 product.post('/', addProduct)
-product.get('/:id', getProductById)
 product.put('/:id', updateProductById)
 product.delete('/:id', deleteProductById)
 
