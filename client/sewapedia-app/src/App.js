477fab78ca6e868cba0fcc60c0be2ef132aa9192
Hendry Yohanes <93873029+OrchaZenn@users.noreply.github.com>
2022-04-18 23:04:59 +0700

    Update App.js

--- client/sewapedia-app/src/App.js
+++ client/sewapedia-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom'
 import About from './Pages/About';
 import Home from './Pages/Home';
 import Products from './Pages/Products/Products';
