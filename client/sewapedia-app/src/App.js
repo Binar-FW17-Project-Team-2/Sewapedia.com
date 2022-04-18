6efe331efee9cbcce8db3fb4ce69613404312832
Muhammad Rizky <mrizky98@gmail.com>
2022-04-18 13:54:27 +0700

    forgotpassword

--- client/sewapedia-app/src/App.js
+++ client/sewapedia-app/src/App.js
@@ -6,6 +6,8 @@ import Home from "./Pages/Home";
 import LoginPage from "./Pages/Auth/LoginPage";
 import FAQ from "./Component/FAQ";
 import RegisterPage from "./Pages/Auth/RegisterPage";
+import ForgotPassword from "./Pages/Auth/ForgotPassword";
+import ResetPassword from "./Pages/Auth/ResetPassword";
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
       <Route path="/faq" element={<FAQ />} />
       <Route path="/signIn" element={<LoginPage />} />
       <Route path="/signUp" element={<RegisterPage />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
+      <Route path="/resetpassword" element={<ResetPassword />} />
     </Routes>
   );
 }
