6c68c9f1f17d4b92651159812451000cdb435ed8
asyhp <aisyahap1595@gmail.com>
2022-04-20 22:28:17 +0700

    feat: menambahkan editUser dan deleteUser

--- server/routers/v1/userRouter.js
+++ server/routers/v1/userRouter.js
@@ -1,11 +1,13 @@
 const user = require("express").Router();
 const { isAuthenticated, isAuthorized } = require("../../middleware");
-const { getUsers, getUserById } = require("../../controller/user");
+const { getUsers, getUserById, editUser, deleteUser } = require("../../controller/user");
 
 user.use(isAuthenticated);
-user.use(isAuthorized([{ role: "admin" }, { role: "user", sameUser: true }]));
+user.use(isAuthorized([{ role: "admin" }, { role: "user" }]));
 user.get("/", getUsers);
+user.post('/edit', editUser);
 user.get("/:id", getUserById);
+user.delete("/:id", deleteUser);
 // implement delete kalo mau ada delete
 
 module.exports = user;
