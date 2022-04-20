97b3196d78cf1d438e6ba39c587f7576a96be9eb
heypurnomo <heypurnomo@gmail.com>
2022-04-21 01:56:10 +0700

    feat: multiple upload image

--- client/sewapedia-app/src/Pages/Products/AddProduct.jsx
+++ client/sewapedia-app/src/Pages/Products/AddProduct.jsx
@@ -8,6 +8,7 @@ import { TextInput } from "../../Component/CustomInput";
 import { validationAddProduct } from "../../utils/validation";
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { storage } from "../../config/firebase";
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function AddProduct() {
   return (
@@ -37,11 +38,13 @@ export default function AddProduct() {
 
 function FormAddProduct() {
   const [image, setImage] = useState(null);
-  const [dwnldUrl, setDwnldUrl] = useState();
+  const [imageUrl, setImageUrl] = useState([]);
   const [progress, setProgress] = useState(0);
-  const [category, setCategory] = useState([])
+  const [category, setCategory] = useState([]);
+  const [errorInput, setErrorInput] = useState(false);
   const navigate = useNavigate();
 
+  
   function inputImage(e) {
     if (!e.target.files) {
       return setImage(undefined);
@@ -81,7 +84,7 @@ function FormAddProduct() {
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-          setDwnldUrl(downloadUrl);
+          setImageUrl(prev => [...prev, downloadUrl]);
         });
       }
     );
@@ -98,6 +101,7 @@ function FormAddProduct() {
       }}
       validationSchema={validationAddProduct}
       onSubmit={async (values) => {
+        if (errorInput) return
         const res = await fetch(`http://localhost:4000/api/v1/product`, {
           method: 'POST',
           credentials: 'include',
@@ -107,7 +111,7 @@ function FormAddProduct() {
             details: values.details,
             stock: values.stock,
             price: values.price,
-            img_url: [dwnldUrl]
+            img_url: imageUrl
           }),
           headers: {"Content-Type": "application/json"}
         })
@@ -128,20 +132,62 @@ function FormAddProduct() {
               display: 'flex',
               flexDirection: 'column',
               p:1,
-              mt: 10,
+              mt: 1,
               mx: 'auto',
               '& .MuiTextField-root': { my: 1, width: '100%'},
             }}
-          >
-            <Box sx={{ width: '100%' }}>
+          > 
+            <Box sx={{width: '100%', height: '100px', overflowX: 'auto', overflowY: 'hidden'}}>
+              <Box sx={{width: 'auto', display:'flex', height:'100%' }}>
+                {
+                  imageUrl.map((value, idx) => (
+                    <Box 
+                      key={idx}
+                      sx={{width: '100px', height:'100%', flexShrink: '0', mx:1, position:'relative'}}
+                    >
+                      <Box
+                        component="img"
+                        sx={{
+                          height: '100px',
+                          width: '100px',
+                        }}
+                        src={value}
+                        loading='lazy'
+                      />
+                      <Button 
+                        variant='contained' 
+                        size="small" 
+                        sx={{
+                          position: 'absolute', 
+                          right:'0', 
+                          p:0, 
+                          minWidth:0
+                        }}
+                        onClick={()=> {
+                          setImageUrl(
+                            prev => prev.filter((val, id) => (id !== idx))
+                          )
+                        }}
+                      >
+                        <CloseIcon fontSize="small"/>
+                      </Button>
+                    </Box>
+                  ))
+                }
+                
+              </Box>
+            </Box>
+            <Box sx={{ width: '100%', mt:2 }}>
               <LinearProgress variant="determinate" value={progress} />
             </Box>
             <TextInput
               id='img_url'
               name='img_url'
               type='file'
-              required
+              inputProps={{accept:"image/*"}}
               onChange={inputImage}
+              {...(errorInput) ? errorInput : null}
+              onClick={() => {setErrorInput(false)}}
             />
             <TextInput 
               id='name'
@@ -184,7 +230,20 @@ function FormAddProduct() {
              <Typography variant="p" sx={{color: 'red'}}>{formik.errors.details}</Typography>
            ) : null}
             
-            <Button sx={{mt:1}} type="submit" color="info" variant="contained">ADD PRODUCT</Button>
+            <Button 
+              sx={{mt:1}} 
+              type="submit" 
+              color="info" 
+              variant="contained"
+              onClick={() => {
+                if (!imageUrl.length) return setErrorInput({
+                  error: true,
+                  helperText: 'wajib upload image'
+                })
+              }}      
+            >
+              ADD PRODUCT
+            </Button>
           </Paper>
 
         )
