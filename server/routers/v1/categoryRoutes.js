880633ef498038ed10f9c8d270436230408ffcc8
Muhammad Rizky <mrizky98@gmail.com>
2022-05-23 00:42:14 +0700

    edit categories page

--- server/routers/v1/categoryRoutes.js
+++ server/routers/v1/categoryRoutes.js
@@ -1,7 +1,8 @@
 const category = require('express').Router();
-const { getCategories, createCategory, updateCategory, deleteCategory } = require('../../controller/category');
+const { getCategories, createCategory, updateCategory, deleteCategory,getCategoryByName } = require('../../controller/category');
 const { isAuthenticated, roleAuthorization  } = require('../../middleware');
 
+category.get('/:name', getCategoryByName);
 category.use(isAuthenticated, roleAuthorization('admin'));
 category.get('/', getCategories);
 category.post('/', createCategory);
